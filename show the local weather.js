67af17461f7a5d6dee5250d32400add3c0c68c34
riversword <2694473261@qq.com>
2017-09-19 18:20:50 +0800

    第7次修改，show the local weather7.0

--- show the local weather.js
+++ show the local weather.js
@@ -1,5 +1,11 @@
-
+//使用的API为和风天气
 var lat,lon;
+var banners=$(' div.banner'),  //用于轮播
+  circles=$('.circleGroup div')
+  banLen=banners.length,
+  j=0,
+  timer=null;
+
 //通过HTML5获取地理位置
 function getLocation(){
   if(navigator.geolocation){
@@ -24,114 +30,104 @@ function showByIp(){
   myIp=returnCitySN["cip"];
   getWeather(myIp);
 }
-
-//获取天气信息，和风天气
+//判断使用什么天气图标
+function weatherPicture(codeNum){
+  switch(codeNum){
+      case '100':
+      return '0.png';
+      break;
+      case '101':
+      return '4.png';
+      break;
+      case '102':
+      return '7.png';
+      break;
+      case '103':
+      return '5.png';
+      break;
+      case '104':
+      return '9.png';
+      break;
+      case '200':case '202':case '203':case '204':case '205':case '206':case '207':case '208':case '209':case '210':case '211':case '212':case '213':
+      return '32.png';
+      break;
+      case '201':
+      return '9.png';
+      break;
+      case '300':case '305':case '309':
+      return '10.png';
+      break;
+      case '301':case '306':
+      return '14.png';
+      break;
+      case '302':case '303':case '304':
+      return '16.png';
+      break;
+      case '307':case '308':case '310':case '311':case '312':
+      return '15.png';
+      break;
+      case '313':case '404':case '405':case '406':
+      return '12.png';
+      break;
+      case '400':case '407':
+      return '21.png';
+      break;
+      case '401':case '402':case '403':
+      return '23.png';
+      break;
+      case '500':case '501':
+      return '31.png';
+      break;
+      case '502':
+      return '30.png';
+      break;
+      case '503':case '504':case '507':case '508':
+      return '26.png';
+      break;
+    default:
+    return '99.png';
+  }
+}
+//获取天气信息
 function getWeather(keyWords){
   var apiUrl="https://free-api.heweather.com/v5/weather?city="+keyWords+'&key=45f089df10e64f3591d88f0a2c6c7dc5';
   $.ajax({
     url:apiUrl,
     type:"get",
     success: function(data){
-      console.log(data);
-     /* var weatherCode=data['HeWeather5']['0']['now']['cond']['code'];
-      switch(weatherCode){
-        case 100: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        case 101: &('#weaIcon img').css("src","../images/weatherIcons/7.png");
-        break;
-        case 102,103: &('#weaIcon img').css("src","../images/weatherIcons/5.png");
-        break;
-        case 104: &('#weaIcon img').css("src","../images/weatherIcons/9.png");
-        break;
-        case 200~213: &('#weaIcon img').css("src","../images/weatherIcons/32.png");
-        break;
-        case 300~313: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        case 400~407: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        case 500~508: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        case 900: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        case 901: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-        break;
-        default: &('#weaIcon img').css("src","../images/weatherIcons/0.png");
-      }*/
+
       //选择器加eq()将jq模式转换为js模式，直接用id选择器也无法对元素进行设置
       $('#content p:eq(1)').html(data['HeWeather5']['0']['now']['cond']['txt']);
+      var nowPicture=weatherPicture(data['HeWeather5']['0']['now']['cond']['code']);
+      $('#content img').attr("src","https://github.com/riversword/images/raw/master/weather/bigweatherIcons/"+nowPicture);
       $('#content p:eq(0) span:eq(0)').html(data['HeWeather5']['0']['now']['tmp']);
       $('#content p:eq(3)').html(data['HeWeather5']['0']['now']['wind']['dir']+ '&nbsp;' +data['HeWeather5']['0']['now']['wind']['sc']);
       lon=data['HeWeather5']['0']['basic']['lon'];
       lat=data['HeWeather5']['0']['basic']['lat'];
       lon=dealNum(lon);
       lat=dealNum(lat);
-      $('#content p:eq(4)').html("lon&nbsp;"+lon+'，'+"lat&nbsp"+lat);
+      $('#content p:eq(4)').html("经&nbsp;"+lon+'，'+"纬&nbsp"+lat);
       $('.poster h3').eq(0).html(data['HeWeather5']['0']['basic']['city'] + '&nbsp;' + data['HeWeather5']['0']['basic']['cnty']);
-      $('.poster p').eq(5).html("update: "+data['HeWeather5']['0']['basic']['update']['loc']);
+      $('.poster p').eq(5).html("更新: "+data['HeWeather5']['0']['basic']['update']['loc']);
       $('#content p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['0']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']+'℃');
-      /* 选择元素时，i被当作一般的字符了；另，['hourly_forecast']数组元素个数不定，不能在HTML中直接写出结构
-      var lenHour=data['HeWeather5']['0']['hourly_forecast'].length;
-      for(var i=0;i<lenHour;i++){
-        $('#more div:eq(i) p:eq(0)').html(data['HeWeather5']['0']['hourly_forecast']['0']['cond']['txt']);
-        $('#more div:eq(i) p:eq(1)').html(data['HeWeather5']['0']['hourly_forecast']['0']['tmp']+"℃");
-        $('#more div:eq(i) p:eq(2)').html(data['HeWeather5']['0']['hourly_forecast']['0']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['hourly_forecast']['0']['wind']['sc']);
-        $('#more div:eq(i) p:eq(3)').html(data['HeWeather5']['0']['hourly_forecast']['0']['date']);
-      }*/
-        $('#more div:eq(0) p:eq(0)').html(data['HeWeather5']['0']['hourly_forecast']['0']['cond']['txt']);
-        $('#more div:eq(0) p:eq(1)').html(data['HeWeather5']['0']['hourly_forecast']['0']['tmp']+"℃");
-        $('#more div:eq(0) p:eq(2)').html(data['HeWeather5']['0']['hourly_forecast']['0']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['hourly_forecast']['0']['wind']['sc']);
-        $('#more div:eq(0) p:eq(3)').html(data['HeWeather5']['0']['hourly_forecast']['0']['date']);
-
-        $('#more div:eq(1) p:eq(0)').html(data['HeWeather5']['0']['hourly_forecast']['1']['cond']['txt']);
-        $('#more div:eq(1) p:eq(1)').html(data['HeWeather5']['0']['hourly_forecast']['1']['tmp']+"℃");
-        $('#more div:eq(1) p:eq(2)').html(data['HeWeather5']['0']['hourly_forecast']['1']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['hourly_forecast']['0']['wind']['sc']);
-        $('#more div:eq(1) p:eq(3)').html(data['HeWeather5']['0']['hourly_forecast']['1']['date']);
-
-     /*   $('#more div:eq(2) p:eq(0)').html(data['HeWeather5']['0']['hourly_forecast']['2']['cond']['txt']);
-        $('#more div:eq(2) p:eq(1)').html(data['HeWeather5']['0']['hourly_forecast']['2']['tmp']+"℃");
-        $('#more div:eq(2) p:eq(2)').html(data['HeWeather5']['0']['hourly_forecast']['2']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['hourly_forecast']['0']['wind']['sc']);
-        $('#more div:eq(2) p:eq(3)').html(data['HeWeather5']['0']['hourly_forecast']['2']['date']); */
-
-
-        $('#suggest p:eq(0)').html(data['HeWeather5']['0']['suggestion']['cw']['txt']);
-        $('#suggest p:eq(1)').html(data['HeWeather5']['0']['suggestion']['drsg']['txt']);
-        $('#suggest p:eq(2)').html(data['HeWeather5']['0']['suggestion']['flu']['txt']);
-        $('#suggest p:eq(3)').html(data['HeWeather5']['0']['suggestion']['sport']['txt']);
-        $('#suggest p:eq(4)').html(data['HeWeather5']['0']['suggestion']['trav']['txt']);
-        $('#suggest p:eq(5)').html(data['HeWeather5']['0']['suggestion']['uv']['txt']);
       
-
-        $('#day .today .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['0']['date']);
-        $('#day .today .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['0']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#day .today .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['0']['cond']['txt_d']);
-        $('#day .today .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['0']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
-
-        $('#day .seconday .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['1']['date']);
-        $('#day .seconday .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['1']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#day .seconday .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['1']['cond']['txt_d']);
-        $('#day .seconday .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['1']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
-
-        $('#day .thirday .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['2']['date']);
-        $('#day .thirday .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['2']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#day .thirday .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['2']['cond']['txt_d']);
-        $('#day .thirday .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['2']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
-
-
-
-        $('#night .today .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['0']['date']);
-        $('#night .today .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['0']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#night .today .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['0']['cond']['txt_n']);
-        $('#night .today .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['0']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
-
-        $('#night .seconday .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['1']['date']);
-        $('#night .seconday .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['1']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#night .seconday .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['1']['cond']['txt_n']);
-        $('#night .seconday .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['1']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
-
-        $('#night .thirday .dateInfo').eq(0).html(data['HeWeather5']['0']['daily_forecast']['2']['date']);
-        $('#night .thirday .weather p:eq(0)').html(data['HeWeather5']['0']['daily_forecast']['2']['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']);
-        $('#night .thirday .weather p:eq(1)').html(data['HeWeather5']['0']['daily_forecast']['2']['cond']['txt_n']);
-        $('#night .thirday .weather p:eq(2)').html(data['HeWeather5']['0']['daily_forecast']['2']['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast']['0']['wind']['sc']);
+      for(var i=0;i<3;i++){
+        $('.dateInfo').eq(i).html(data['HeWeather5']['0']['daily_forecast'][i]['date'].slice(5));
+        $('.weather').html("<p>"+data['HeWeather5']['0']['daily_forecast'][i]['tmp']['min']+"~"+data['HeWeather5']['0']['daily_forecast']['0']['tmp']['max']+"℃</p><p>日:"+data['HeWeather5']['0']['daily_forecast'][i]['cond']['txt_d']+"</p><p>夜:"+data['HeWeather5']['0']['daily_forecast'][i]['cond']['txt_n']+"</p><p>"+data['HeWeather5']['0']['daily_forecast'][i]['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['daily_forecast'][i]['wind']['sc']+"</p>");
+        var dailyPicture=weatherPicture(data['HeWeather5']['0']['daily_forecast'][i]['cond']['code_d']);
+        $('.weaIcon img').eq(i).attr("src", "https://github.com/riversword/images/raw/master/weather/bigweatherIcons/"+dailyPicture);
+       }
+      $('.suggest').eq(0).append("<p>"+data['HeWeather5']['0']['suggestion']['air']['txt']+"</p><p>"+data['HeWeather5']['0']['suggestion']['cw']['txt']+"</p><p>"+data['HeWeather5']['0']['suggestion']['drsg']['txt']+"</p><p>"+data['HeWeather5']['0']['suggestion']['sport']['txt']+"</p><p>"+data['HeWeather5']['0']['suggestion']['trav']['txt']+"</p><p>"+data['HeWeather5']['0']['suggestion']['uv']['txt']+"</p>");
+      
+       var lenHour=data['HeWeather5']['0']['hourly_forecast'].length,
+           coluWid=Math.floor(510/lenHour);
+       for(var i=0;i<lenHour;i++){
+        $('.coluCover').eq(0).append("<div class='colu'><img><p>"+data['HeWeather5']['0']['hourly_forecast'][i]['cond']['txt']+"</p><p>"+data['HeWeather5']['0']['hourly_forecast'][i]['tmp']+"℃</p><p>"+data['HeWeather5']['0']['hourly_forecast'][i]['wind']['dir']+"&nbsp;"+data['HeWeather5']['0']['hourly_forecast'][i]['wind']['sc']+"</p><p>"+data['HeWeather5']['0']['hourly_forecast'][i]['date'].slice(11)+"</p></div>");
+        var hourlyPicture=weatherPicture(data['HeWeather5']['0']['hourly_forecast'][i]['cond']['code']);
+        $('.colu img').eq(i).attr("src","https://github.com/riversword/images/raw/master/weather/smallweatherIcons/"+hourlyPicture);
+        $('.colu').css("width",coluWid+"px");
+       }
+         
     },
     error:function(){
       alert("获取天气失败");
@@ -146,36 +142,54 @@ function dealNum(x){
     return a.slice(0,b+3);
    }else return x;
 }
-var numS=0,numD;
-$('input[name="switchButton"]').click(function(){
-  if(numS==0){
-    $('#more').slideUp("fast");
-    $('#suggest').slideDown("slow");
-    $('input[name="title"]').val("Advices in this weather");
-    $('input[name="switchButton"]').val("forecast");
-    numS=1;
-  }else{
-    $('#suggest').slideUp("fast");
-    $('#more').slideDown("slow");
-    $('input[name="title"]').val("Hourly forecast for today");
-    $('input[name="switchButton"]').val("suggest");
-    numS=0;
-  }
-});
-//为什么最开始，这个要click两次才能生效？
-$('input[name="dayNight"]').click(function(){
-  if(numD==0){
-    $('#day').slideUp("fast");
-    $('#night').slideDown("slow");
-    $('input[name="dayNight"]').val("day");
-    numD=1;
-  }else{
-    $('#night').slideUp("fast");
-    $('#day').slideDown("slow");
-    $('input[name="dayNight"]').val("night");
-    numD=0;
-  }
-});
+
 getLocation();
 
+//实现轮播
+function autoPlay(){
+  timer=setInterval(function(){
+    changeItem();
+    j++;
+  },4000);
 
+}
+function changeItem(){
+  if(j>banLen-1){
+    j=0;
+  }
+  for(var i=0;i<banLen;i++){
+    banners[i].style.display="none";
+    circles.eq(i).css("background-color","rgba(117,127,137,0.3)");
+  }
+  banners[j].style.display="block";
+  circles.eq(j).css("background-color","rgba(250,250,250,0.6)");;
+}
+function stopPlay(){
+  clearInterval(timer);
+}
+$('div.bannerBox').mouseenter(stopPlay);
+$('div.bannerBox').mouseleave(autoPlay);
+$('#pre').click(function(){  //为何第一次点击无效
+  if(j==0){
+    j=banLen-1
+  }else{
+    --j;
+  }
+  changeItem();
+});
+$('#nex').click(function(){
+  if(j==banLen-1){
+    j=0;
+  }else{
+    ++j;
+  }
+  changeItem();
+});
+for(var i=0;i<circles.length;i++){
+  circles[i].id=i;
+  circles[i].onclick=function(){
+    j=this.id;
+    changeItem();
+  }
+}
+autoPlay();
